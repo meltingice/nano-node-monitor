d289728f3e5ecc241468218b2e12a7ca530db295
Ryan LeFevre <meltingice8917@gmail.com>
2018-03-20 17:50:43 -0400

    Get initial paths and components in place

--- app/javascript/app/App.js
+++ app/javascript/app/App.js
@@ -1,14 +1,19 @@
 import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import Layout from './Layout'
+import NodeInfo from './views/NodeInfo'
+import Accounts from './views/Accounts'
+import Logs from './views/Logs'
 
 export default class App extends React.Component {
   render() {
     return (
       <Router>
         <Layout>
-
+          <Route exact path="/" component={NodeInfo} />
+          <Route exact path="/accounts" component={Accounts} />
+          <Route exact path="/logs" component={Logs} />
         </Layout>
       </Router>
     )
